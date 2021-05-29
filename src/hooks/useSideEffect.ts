4208ccf35a791df050f5592335a6fd6b3861a5da
ncpa0cpl <szymonb21@gmail.com>
2021-05-29 15:34:09 +0200

    fix: useSideEffect not running the effect when an empty array passed as deps

--- src/hooks/useSideEffect.ts
+++ src/hooks/useSideEffect.ts
@@ -3,8 +3,8 @@ import { compareArrays } from "../libs";
 import { UnitScopeManager } from "../unit";
 
 export function useSideEffect(effect: () => void, deps?: any[]) {
-  const prevDeps = useReference<any[]>([]);
-  if (!deps || !compareArrays(deps, prevDeps.current)) {
+  const prevDeps = useReference<any[] | undefined>(undefined);
+  if (!deps || !prevDeps.current || !compareArrays(deps, prevDeps.current)) {
     UnitScopeManager.deferAction(effect);
     if (deps) prevDeps.current = deps;
   }
